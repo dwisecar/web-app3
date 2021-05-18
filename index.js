0d7511ece194a7b0515bffa2d3cda11dac8e05d5
David Wisecarver <46696828+dwisecar@users.noreply.github.com>
2021-05-18 14:04:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -44,6 +44,10 @@ app.use(
     authRequired: false,
     auth0Logout: true,
     baseURL: APP_URL,
+    authorizationParams: {
+     response_type: "code id_token",
+     audience: "https://expenses-api",
+   },
   })
 );
 
@@ -70,15 +74,23 @@ app.get("/user", requiresAuth(), async (req, res) => {
 });
 
 app.get("/expenses", requiresAuth(), async (req, res, next) => {
-  try {
-    const expenses = await axios.get(`${API_URL}/reports`);
-    res.render("expenses", {
-      user: req.oidc && req.oidc.user,
-      expenses: expenses.data,
-    });
-  } catch (err) {
-    next(err);
-  }
+ try {
+   // 👇 get the token from the request 👇
+   const { token_type, access_token } = req.oidc.accessToken;
+   // 👇 then send it as an authorization header 👇
+   const expenses = await axios.get(`${API_URL}/reports`, {
+     headers: {
+       Authorization: `${token_type} ${access_token}`,
+     },
+   });
+   // 👆 end of changes 👆
+   res.render("expenses", {
+     user: req.oidc && req.oidc.user,
+     expenses: expenses.data,
+   });
+ } catch (err) {
+   next(err);
+ }
 });
 
 // catch 404 and forward to error handler
